5855d25b18bdfdcb3f281a91f47226cda873f414
WayHacker <nik.zernov.04@mail.ru>
2024-11-03 00:06:42 +0300

    Add two routes/set cookie/get cookie

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, Cookie
+import string
+import random
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/set_cookie")
+def set_cookie(response: Response):
+    response.set_cookie(
+        key="special_cookie",
+        value="".join(
+            random.choices(string.ascii_letters, k=26)
+        ),  # "random" string sequence
+    )
+    return {"message": "Cookie now set!"}
+
+
+@app.get("/get_cookie")
+def get_cookie(special_cookie: str = Cookie(None)):
+    return {"special_cookie": special_cookie}  # returns cookie value
